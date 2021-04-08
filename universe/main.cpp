b3fb0b8d06b7aa0bb24c42cbbe221370ac8da6df
dynamic21 <21dynamicjustin@gmail.com>
2021-04-08 11:40:44 -0700

    ok

--- universe/main.cpp
+++ universe/main.cpp
@@ -6,6 +6,7 @@ using olc::vd2d;
 using olc::Pixel;
 
 using std::max;
+using std::min;
 using std::endl;
 using std::cout;
 using std::vector;
@@ -15,8 +16,8 @@ using std::chrono::microseconds;
 using std::chrono::duration_cast;
 using std::chrono::high_resolution_clock;
 
-#define gravity 100
-#define userFriction 0.1
+#define gravity 1
+#define controlFriction 0.1
 
 class Example : public olc::PixelGameEngine
 {
@@ -27,16 +28,17 @@ public:
 	}
 
 public:
+	vd2d pos;
+	vd2d posv;
+	double zoom;
+
+	vd2d halfScreen;
 	unsigned int m_z;
 	unsigned int m_w;
+
 	vector<vd2d> poss;
 	vector<vd2d> posvs;
-	vector<vd2d> poscs;
-	vector<int> size;
-	double zoom;
-	vd2d posv;
-	vd2d pos;
-	vd2d halfScreen;
+	vector<Pixel> colors;
 
 	unsigned int intRand()
 	{
@@ -46,81 +48,18 @@ public:
 		return (m_z << 16) + m_w;
 	}
 
-	double doubleRand()
+	double doubleRand() { return (intRand() + 1.0) * 2.328306435454494e-10; }
+
+	Pixel mapToRainbow(double d) {
+		double r = (d > 3) ? max(0.0, min(1.0, d - 4)) : max(0.0, min(1.0, 2 - d));
+		double g = (d > 2) ? max(0.0, min(1.0, 4 - d)) : max(0.0, min(1.0, d));
+		double b = (d > 4) ? max(0.0, min(1.0, 6 - d)) : max(0.0, min(1.0, d - 2));
+
+		return Pixel(r * 0xff, g * 0xff, b * 0xff);
+	}
+
+	void control(float fElapsedTime)
 	{
-		return (intRand() + 1.0) * 2.328306435454494e-10;
-	}
-
-	void applyForce(float fElapsedTime, bool on) {
-
-		for (int i = 0; i < posvs.size() - 1; i++)
-		{
-			vd2d mpos = poss[i];
-			vd2d mposv = posvs[i];
-			int mSize = size[i];
-			for (int j = i + 1; j < posvs.size(); j++)
-			{
-				int totalSize = mSize + size[j];
-				vd2d dpos = poss[j] - mpos;
-				double dis = dpos.mag2();
-
-				/*DrawLine((mpos - pos) * zoom + halfScreen, (mpos + (dpos / dis * gravity) - pos) * zoom + halfScreen, olc::RED);
-				DrawLine((poss[j] - pos) * zoom + halfScreen, (poss[j] - (dpos / dis * gravity) - pos) * zoom + halfScreen, olc::RED);*/
-
-				if (on) {
-					/*cout << "ball " << i << " and ball " << j << " is " << dpos << endl;
-					cout << "ball " << i << " is " << mpos << endl;
-					cout << "ball " << j << " is " << poss[j] << endl;
-					cout << "dis " << dis << endl;
-					cout << "dpos / dis " << (dpos / dis) << endl;
-					cout << "gravity * dpos / dis " << (dpos / dis * gravity) << endl;
-					cout << endl;*/
-					mposv += dpos / dis * gravity * fElapsedTime;
-					posvs[j] -= dpos / dis * gravity * fElapsedTime;
-
-					if (dpos.x < totalSize && dpos.y < totalSize) {
-						if (totalSize * totalSize > dis) {
-
-							dis = sqrt(dis);
-							double depth = dis - totalSize;
-							vd2d dposv = posvs[j] - mposv;
-							vd2d npos = dpos / dis;
-							double mag = dposv.dot(npos);
-
-							if (mag <= 0)
-							{
-
-								//cout << "mag before: " << mag << " and depth before: " << depth << endl;
-								//depth = max(mag, depth);
-								//mag -= depth;
-								//cout << "mag after: " << mag << " and depth after: " << depth << endl;
-
-								vd2d dapply = npos * mag * 0.9;
-								mposv += dapply;
-								posvs[j] -= dapply;
-
-								dapply = npos * (depth / 2);
-								poscs[i] += dapply;
-								poscs[j] -= dapply;/**/
-							}
-						}
-					}
-				}
-			}
-			posvs[i] = mposv;
-		}
-	}
-
-	void move(float fElapsedTime) {
-		for (int i = 0; i < poss.size(); i++)
-		{
-			poss[i] = poss[i] + poscs[i] + (posvs[i] * fElapsedTime);
-			poscs[i] = { 0,0 };
-		}
-	}
-
-	void user(float fElapsedTime) {
-		//fElapsedTime /= 10;
 		if (GetKey(Key::Q).bHeld) { zoom /= pow(2, fElapsedTime); }
 		if (GetKey(Key::E).bHeld) { zoom *= pow(2, fElapsedTime); }
 
@@ -129,57 +68,75 @@ public:
 		if (GetKey(Key::S).bHeld || GetKey(Key::DOWN).bHeld) { posv.y += 1000 / zoom * fElapsedTime; }
 		if (GetKey(Key::D).bHeld || GetKey(Key::RIGHT).bHeld) { posv.x += 1000 / zoom * fElapsedTime; }
 
-		posv *= pow(userFriction, fElapsedTime);
+		posv *= pow(controlFriction, fElapsedTime);
 		pos += posv * fElapsedTime;
 	}
 
-	void drawScreen(float fElapsedTime) {
+	void collision(float fElapsedTime)
+	{
+		for (int i = 0; i < posvs.size() - 1; i++)
+		{
+			vd2d mposv = posvs[i];
+
+			for (int j = i + 1; j < posvs.size(); j++)
+			{
+				vd2d dpos = poss[j] - poss[i];
+				double dis = dpos.mag2();
+
+				mposv += dpos / dis * gravity * fElapsedTime;
+				posvs[j] -= dpos / dis * gravity * fElapsedTime;
+
+				if (dis < 4) {
+
+					dis = sqrt(dis);
+					vd2d dposv = posvs[j] - mposv;
+					vd2d npos = dpos / dis;
+					double mag = dposv.dot(npos);
+
+					if (mag < 0) {
+						vd2d dapply = npos * mag;
+						mposv += dapply;
+						posvs[j] -= dapply;
+					}
+				}
+			}
+			posvs[i] = mposv;
+		}
+	}
+
+	void drawScreen(float fElapsedTime)
+	{
 		Clear(Pixel(0, 0, 0));
 
 		for (int i = 0; i < poss.size(); i++)
 		{
-			DrawCircle((poss[i] - pos) * zoom + halfScreen, size[i] * zoom);
-			//DrawLine((poss[i] - pos) * zoom + halfScreen, (poss[i] + posvs[i] - pos) * zoom + halfScreen);
+			poss[i] = poss[i] + posvs[i] * fElapsedTime;
+			FillCircle((poss[i] - pos) * zoom + halfScreen, zoom, colors[i]);
 		}
 	}
 
 	bool OnUserCreate() override
 	{
+		zoom = 16;
 		halfScreen = { (double)ScreenWidth() / 2, (double)ScreenHeight() / 2 };
 		m_z = (unsigned int)duration_cast<seconds>(high_resolution_clock::now().time_since_epoch()).count();
 		m_w = (unsigned int)duration_cast<microseconds>(high_resolution_clock::now().time_since_epoch()).count();
-		zoom = (double)1 / 1;
+
 		for (int i = 0; i < 2000; i++)
 		{
-			poss.push_back((vd2d{ doubleRand() * ScreenWidth(), doubleRand() * ScreenHeight() } - halfScreen) * 10);
-			posvs.push_back(vd2d{ doubleRand() * 200 - 100, doubleRand() * 200 - 100 });
-			poscs.push_back(vd2d{ 0,0 });
-			size.push_back(100);
-		}/**/
-		/*poss.push_back(vd2d{ 300, 0 });
-		posvs.push_back(vd2d{ 0, 0 });
-		poscs.push_back(vd2d{ 0,0 });
-		size.push_back(100);
-		poss.push_back(vd2d{ 0, 0 });
-		posvs.push_back(vd2d{ 0, 0 });
-		poscs.push_back(vd2d{ 0,0 });
-		size.push_back(100);
-		poss.push_back(vd2d{ 0, 100 });
-		posvs.push_back(vd2d{ 0, 100 });
-		poscs.push_back(vd2d{ 0,0 });
-		size.push_back(100);*/
+			poss.push_back((vd2d{ doubleRand() * ScreenWidth(), doubleRand() * ScreenHeight() } - halfScreen));
+			posvs.push_back(vd2d{ 0, 0 });
+			colors.push_back(mapToRainbow(doubleRand() * 6));
+		}
+
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		user(fElapsedTime);
-		drawScreen(fElapsedTime);
-		//GetKey(Key::SPACE).bHeld
-		applyForce(fElapsedTime, GetKey(Key::SPACE).bHeld);
-		if (GetKey(Key::SPACE).bHeld) {
-			move(fElapsedTime);
-		}
+		control(fElapsedTime);
+		collision(0.01); // defined until stable fps
+		drawScreen(0.01); // defined until stable fps
 
 		return true;
 	}
@@ -188,7 +145,7 @@ public:
 int main()
 {
 	Example demo;
-	if (demo.Construct(1000, 1000, 1, 1))
+	if (demo.Construct(500, 500, 2, 2))
 		demo.Start();
 	return 0;
 }
